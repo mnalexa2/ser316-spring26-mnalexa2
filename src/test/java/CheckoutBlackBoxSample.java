dda2af316a49913e468c949995dfba98d69b3298
Michelle Alexander <mnnalexa2@asu.edu>
2026-02-03 01:30:33 -0700

    Commit change of comment code in CheckoutBlackBoxSample.java

--- src/test/java/CheckoutBlackBoxSample.java
+++ src/test/java/CheckoutBlackBoxSample.java
@@ -31,19 +31,19 @@ public class CheckoutBlackboxSample {
      * Each test will run against ALL implementations.
      */
     @SuppressWarnings("unchecked")
-    static Stream<Class<? extends Checkout>> checkoutClassProvider() {
-        return (Stream<Class<? extends Checkout>>) Stream.of(
-                Checkout0.class,
-                Checkout1.class,
-                Checkout2.class,
-                Checkout3.class
-        );
-    }
-
-    // Uncomment when you implement the method in assign 3 and comment the above
 //    static Stream<Class<? extends Checkout>> checkoutClassProvider() {
-//        return Stream.of(Checkout.class);
+//        return (Stream<Class<? extends Checkout>>) Stream.of(
+//                Checkout0.class,
+//                Checkout1.class,
+//                Checkout2.class,
+//                Checkout3.class
+//        );
 //    }
+
+    //Uncomment when you implement the method in assign 3 and comment the above
+    static Stream<Class<? extends Checkout>> checkoutClassProvider() {
+        return Stream.of(Checkout.class);
+    }
 
 
     /**
